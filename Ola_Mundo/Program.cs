4ee435bc94bda2e0e14e1c691b172014a3a74c2b
Wagner Bernardo <wagber.dev@gmail.com>
2022-01-09 13:08:26 -0300

    Segundo Commit

--- Ola_Mundo/Program.cs
+++ Ola_Mundo/Program.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+
+//Comentário de uma linha
+/*
+  Comentário de várias linhas
+ */
+
+/*
+using System; -> A using Systemlinha significa que você está usando a Systembiblioteca em seu projeto.
+O que fornece algumas classes e funções úteis, como Consoleclass ou a WriteLinefunção / método.
+
+using System.Collections.Generic; -> Contém interfaces e classes que definem coleções genéricas, que permitem
+aos usuários criar coleções fortemente tipadas que oferecem melhor desempenho e segurança de tipos do que coleções
+fortemente tipadas não genéricas.
+
+using System.Linq; ->
+using System.Text; ->
+using System.Threading.Tasks; ->
+using System.Globalization; ->
+ */
 
 namespace Ola_Mundo
 {
@@ -14,3 +34,12 @@ namespace Ola_Mundo
         }
     }
 }
+
+/*
+Com quebra de linha
+    Console.WriteLine("Olá Mundo!");
+
+Sem quebra de linha
+    Console.Write("Olá ");
+    Console.Write("Mundo!");
+ */
