26ad76e5fe90552e170daa28fe98e5599c76332a
Wagner Bernardo <wagber.dev@gmail.com>
2022-01-09 11:22:26 -0300

    Adicionar arquivos de projeto.

---
+++ Ola_Mundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
